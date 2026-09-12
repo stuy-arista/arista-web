281062ff3e36e651a6fe26838a1c9cde02b0613f
Mekot Sarder <mekotsarder@gmail.com>
2026-09-11 20:05:39 -0400

    Load leaderboard names from public view

--- src/routes/(protected)/leaderboard/+page.server.ts
+++ src/routes/(protected)/leaderboard/+page.server.ts
@@ -3,8 +3,7 @@ import type { RecievedCredit, RecievedPublicUserData } from '$lib/db_types';
 
 export const load = (async ({ locals }) => {
     const users = await locals.pb.collection('publicUsers').getFullList({
-        sort: '-created',
-        fields: 'id,name' 
+        fields: 'id,name'
     }) as unknown as RecievedPublicUserData[];
 
     const allCredits = await locals.pb.collection('credits').getFullList() as unknown as RecievedCredit[];
